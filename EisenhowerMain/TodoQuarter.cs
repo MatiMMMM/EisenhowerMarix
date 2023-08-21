0fc61492e3c23d428dda4fa94c2acc664d1cb439
Mateusz Miałszygrosz <118621056+MatiMMMM@users.noreply.github.com>
2023-08-21 16:43:36 +0200

    Update TodoQuarter.cs
    Added Quaters

--- EisenhowerMain/TodoQuarter.cs
+++ EisenhowerMain/TodoQuarter.cs
@@ -1,11 +1,59 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace EisenhowerCore { 
+namespace Eisenhower_Matrix
+{
+    internal class ToDoQuarter
+    {
 
-    public class TodoQuarter {
+        private List<string> ToDoItems = new List<string>();
+        ToDoItem todoItem = new ToDoItem();
+
+        public ToDoQuarter()
+        {
+            List<string> ToDoQuarter = new List<string>();
+        }
+
+        public void AddItem(string title, string deadline)
+        {
+            var toDoItem = todoItem.CreateToDoItem(title, deadline);
+            ToDoItems.Add(toDoItem);
+        }
+
+        public void RemoveItem(int index)
+        {
+
+        }
+        public void ArchiveItems()
+        {
+
+        }
+
+        public void GetItem(int index)
+        {
+
+        }
+
+        public void GetItems()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < ToDoItems.Count; i++)
+            {
+                sb.AppendLine($"{i + 1}. {ToDoItems[i]}");
+            }
+            return sb.ToString();
+        }
 
     }
-
 }
